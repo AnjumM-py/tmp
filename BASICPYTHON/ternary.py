4a25f48b7847be9eef5d26f727f34d03a0bf8410
AnjumM-py / tmp <anjshaik@gmail.com>
2024-12-07 16:03:22 +0000

    task02

---
+++ BASICPYTHON/ternary.py
@@ -0,0 +1,13 @@
+x = 50
+y = 20
+print( "x is greater" if x>y else " y is greater" )
+
+# if your age is greater then 18 you are allowed to vote
+
+user_age = (int(input("Enter your age: ")))
+if user_age > 18:
+    print( " you are eligible to vote")
+else:
+    print( " you are not eligible to vote")
+
+
