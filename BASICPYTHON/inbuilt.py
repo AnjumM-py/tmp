8eee7daed240d4b221a46391e39babc3aed68e37
AnjumM-py / tmp <anjshaik@gmail.com>
2024-12-07 15:53:41 +0000

    task02

---
+++ BASICPYTHON/inbuilt.py
@@ -0,0 +1,22 @@
+q,r = divmod(10,2)
+print(q)
+print(r)
+
+a ,b , c =10,30,60
+print(a)
+print(b)
+print(c)
+#increment and decrement operator
+x = 9
+x += 3
+print(x)
+
+y = 9
+y -= 3
+print(y)
+
+a = 10
+a *= 3
+print(a)
+
+
